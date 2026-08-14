4e7837bc4486e887c92ac2eedae2ac066a6ce7e2
Izz Abdallah <x3zoabdullah@gmail.com>
2026-08-14 10:51:29 +0400

    imported passport to app.ts and added name to global user in passport.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'dotenv/config'
+import './config/passport.js'
 
 const app = express();
 
--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -11,6 +11,7 @@ declare global{
             id:string
             email:string
             role:string
+            name:string
         }
     }
 }
